f018851496395b31c907ff2343050fb584440b54
efy26 <eliezerefoyaefy2018@gmail.com>
2024-04-26 16:25:10 -0400

    second commit

--- js/script.js
+++ js/script.js
@@ -3,6 +3,13 @@ const btn_menu = document.querySelector(".container_nav_bar p");
 const open_list_menu = document.querySelector(".menu_list_elements");
 const menu_select = document.querySelector(".select_menu");
 const menu_no_select = document.querySelector(".menu_no_select");
+const link_rdv = document.querySelector("#nav_link");
+const contacts = document.querySelector(".contacts");
+const nav_links = document.querySelector("#nav_links");
+const link_rdv_classic = document.querySelector("#nav_link_classic");
+const contacts_classic = document.querySelector(".contacts_menu_classic");
+const nav_links_classic = document.querySelector("#nav_links_classic");
+
  // ========= and variables for menu ===========
 
   // ========= variables for slide ===========
@@ -10,23 +17,40 @@ const menu_no_select = document.querySelector(".menu_no_select");
  let indice = 1;
 
  const slideAu = (valeur) => {
+    
+    
     valeur.classList.add("menu_active")
+    // valeur.classList.remove("menu_disactive");
+   
 } 
 const slideDi = (valeur) => {
     valeur.classList.remove("menu_active")
+    // valeur.classList.add("menu_disactive")
+    // valeur.classList.remove("menu_active")
+
+    
+    
+    // valeur.classList.remove("menu_active")
 } 
  // ========= and variables for slide ===========
 
 window.onload = () => {
-    slideAu(carousel[indice])
+    // slideAu(carousel[indice])
+    // slideDi(carousel[indice])
     // ========= evenements for menu ===========
     btn_menu.addEventListener("click", forOpenMenu)
     menu_select.addEventListener("click", forCloseMenu)
+    link_rdv.addEventListener("click", forOpenMenuRdv)
+    nav_links.addEventListener("click", forCloseMenuRdv)
+    // =======
+    link_rdv_classic.addEventListener("click", forOpenMenuRdv_classic)
+    nav_links_classic.addEventListener("click", forCloseMenuRdv_classic)
      // ========= and evenements for menu ===========
 
     // ========= evenements for slide ===========
-     setInterval(imgSlide, 3000)
-     // ========= evenements for slide ===========
+setInterval(imgSlide, 5200)
+document.querySelector(".bi-instagram").addEventListener("click", imgSlide)
+// ========= evenements for slide ===========
      
 }
 
@@ -35,13 +59,38 @@ const forOpenMenu = (e) => {
     e.preventDefault();
     menu_select.style=" display: unset;";
     menu_no_select.style="display: none;";
-    document.querySelector(".menu_list_elements").style="height: 475px; overflow: scroll;"
+    document.querySelector(".menu_list_elements").style="height: 450px; overflow: scroll;"
 }
 const forCloseMenu = (e) => {
     e.preventDefault();
     menu_select.style="display: none;";
     menu_no_select.style="display: unset;";
     document.querySelector(".menu_list_elements").style="height: 0px; overflow: hidden;"
+}
+const forOpenMenuRdv = (e) => {
+    e.preventDefault();
+    nav_links.style=" display: unset;";
+    link_rdv.style="display: none;";
+    contacts.style="height: 40px; overflow: unset;"
+}
+const forCloseMenuRdv = (e) => {
+    e.preventDefault();
+    nav_links.style="display: none;";
+    link_rdv.style="display: unset;";
+    contacts.style="height: 0px; overflow: hidden; padding: 0px 0px;"
+}
+// ============
+const forOpenMenuRdv_classic = (e) => {
+    e.preventDefault();
+    nav_links_classic.style=" display: unset;";
+    link_rdv_classic.style="display: none;";
+    contacts_classic.style="height: 40px; overflow: unset;"
+}
+const forCloseMenuRdv_classic = (e) => {
+    e.preventDefault();
+    nav_links_classic.style="display: none;";
+    link_rdv_classic.style="display: unset;";
+    contacts_classic.style="height: 0px; overflow: hidden;"
 }
 // ========= and functions for menu ===========
 
@@ -51,21 +100,25 @@ const imgSlide = () => {
     slideDi(carousel[indice])
     slideAu(carousel[indice+1])
 
+
     indice = indice+1
+    slideDi(carousel[4])
+    slideAu(carousel[4])
+        if (indice == 4 ) {
+            indice = 1;
+            
+    
+            slideDi(carousel[indice])
+            slideAu(carousel[indice])
+            // slideDi(carousel[indice])
+            
+            indice = indice
+        }
 
-    if (indice == 4 ) {
-        indice = 1;
 
-        slideDi(carousel[indice])
-        slideAu(carousel[indice])
-        
-        indice = indice
-    }
 }
+
 // ========= and functions for slide ===========
 
-// =========  images ===========
-    
 
-// =========  and images ===========
 
